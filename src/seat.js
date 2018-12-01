51cd94db55eaa58d1154b7a745b78833e2c69fcc
gilesv <viniciusgcp@gmail.com>
2018-12-01 16:27:54 -0300

    Add server and seat scripts

--- src/seat.js
+++ src/seat.js
@@ -3,4 +3,17 @@ const mqtt = require('mqtt');
 const seat = mqtt.connect('mqtt://broker.hivemq.com');
 const topic = '#SITDOWN';
 
-seat.publish()
+seat.publish(topic, {
+    action: 'SEAT_REGISTER',
+    avaliable: true
+});
+
+seat.publish(topic, {
+    action: 'SEAT_UPDATE',
+    available: true
+});
+
+seat.publish(topic, {
+    action: 'SEAT_UPDATE',
+    available: false
+});
