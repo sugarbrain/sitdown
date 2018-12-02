70a5155be7aaab468146d024c947774cb743c272
gilesv <viniciusgcp@gmail.com>
2018-12-02 11:33:08 -0300

    refactoring

--- src/seat.js
+++ src/seat.js
@@ -1,46 +1,79 @@
 const mqtt = require('mqtt');
-
-const seat = mqtt.connect('mqtt://broker.hivemq.com');
-const topic = 'SITDOWN';
+const Gpio = require('pigpio').Gpio;
 
 console.log('Connected.');
 
-const Gpio = require('pigpio').Gpio;
+class Seat {
+  constructor() {
+    this.distance = 0;
+    this.available = true;
+    this.firstTick = true;
+    this.connection = {
+      broker: mqtt.connect('mqtt://broker.hivemq.com'),
+      topic: 'SITDOWN'
+    };
+    
+    this.history = [];
+  }
 
-// The number of microseconds it takes sound to travel 1cm at 20 degrees celcius
-const MICROSECDONDS_PER_CM = 1e6/34321;
-
-const trigger = new Gpio(23, {mode: Gpio.OUTPUT});
-const echo = new Gpio(24, {mode: Gpio.INPUT, alert: true});
-
-trigger.digitalWrite(0); // Make sure trigger is low
-
-let initialTick = true;
-
-const watchHCSR04 = () => {
-  let startTick;
-
-  echo.on('alert', (level, tick) => {
-    if (level == 1) {
-      startTick = tick;
-    } else {
-      const endTick = tick;
-      const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
-      const dist = diff / 2 / MICROSECDONDS_PER_CM;
-      console.log(dist);
-      seat.publish(topic, JSON.stringify({
-         action: initialTick ? 'SEAT_REGISTER' : 'SEAT_UPDATE',
-         distance: dist
-      }));
-      initialTick = false;
+  logHistory(dist) {
+    if (this.history.length > 5) {
+      this.history.slice(1);
     }
-  });
-};
 
-watchHCSR04();
+    this.history.push(dist);
+  }
 
-// Trigger a distance measurement once per second
-setInterval(() => {
-  trigger.trigger(10, 1); // Set trigger high for 10 microseconds
-}, 1000);
+  averageDist() {
+    return this.history.reduce((x, y) => x + y) / this.history.length;
+  }
 
+  init() {
+    const MICROSECDONDS_PER_CM = 1e6/34321;
+    const trigger = new Gpio(23, { mode: Gpio.OUTPUT });
+    const echo = new Gpio(24, { mode: Gpio.INPUT, alert: true });
+
+    trigger.digitalWrite(0);
+
+    const watchHCSR04 = () => {
+      let startTick;
+
+      echo.on('alert', (level, tick) => {
+        if (level == 1) {
+          startTick = tick;
+        } else {
+          const endTick = tick;
+          const diff = (endTick >> 0) - (startTick >> 0); // Unsigned 32 bit arithmetic
+          const dist = diff / 2 / MICROSECDONDS_PER_CM;
+
+          console.log(dist);
+          this.logHistory(dist);
+          this.checkState(dist);
+          this.firstTick = false;
+        }
+      });
+    };
+
+    watchHCSR04();
+
+    // Trigger a distance measurement once per second
+    setInterval(() => {
+      trigger.trigger(10, 1); // Set trigger high for 10 microseconds
+    }, 1000);
+
+  }
+
+  checkState() {
+    if ((this.averageDistance() >= 20.0 && !this.available) || (this.averageDistance() < 20.0 && this.available)) {
+      this.available = !this.available;
+
+      seat.publish(topic, JSON.stringify({
+        action: this.firstTick ? 'SEAT_REGISTER' : 'SEAT_UPDATE',
+        available: this.available
+      }));
+    }
+  }
+}
+
+let seat = new Seat();
+seat.init();
